342ed73cd424e0784cb084b5d84ff17d4c84e214
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-11-22 14:54:55 +0800

    use association instead of direct query

--- test/pay_all_test.rb
+++ test/pay_all_test.rb
@@ -11,12 +11,12 @@ class PayAllTest < Minitest::Test
     data = { item1.id => 3, item2.id => 3}
 
     in_sandbox do
-      UserItem.where(user_id: user.id).atomically.pay_all([:count], data, primary_key: :item_id)
+      user.user_items.atomically.pay_all([:count], data, primary_key: :item_id)
       assert_equal [[item1.id, 8, 4]], user.items.pluck(:item_id, :count, :count_in_bag)
     end
 
     in_sandbox do
-      UserItem.where(user_id: user.id).atomically.pay_all([:count, :count_in_bag], data, primary_key: :item_id)
+      user.user_items.atomically.pay_all([:count, :count_in_bag], data, primary_key: :item_id)
       assert_equal [[item1.id, 8, 4]], user.items.pluck(:item_id, :count, :count_in_bag)
     end
   end
@@ -28,12 +28,12 @@ class PayAllTest < Minitest::Test
     data = { item1.id => 2, item2.id => 2}
 
     in_sandbox do
-      UserItem.where(user_id: user.id).atomically.pay_all([:count], data, primary_key: :item_id)
+      user.user_items.atomically.pay_all([:count], data, primary_key: :item_id)
       assert_equal [[item1.id, 1, 2], [item2.id, 3, 5]], user.items.pluck(:item_id, :count, :count_in_bag)
     end
 
     in_sandbox do
-      UserItem.where(user_id: user.id).atomically.pay_all([:count, :count_in_bag], data, primary_key: :item_id)
+      user.user_items.atomically.pay_all([:count, :count_in_bag], data, primary_key: :item_id)
       assert_equal [[item1.id, 1, 0], [item2.id, 3, 3]], user.items.pluck(:item_id, :count, :count_in_bag)
     end
   end
