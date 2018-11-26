aad6d987049d9e7635be5d319e5864f16010b86f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-11-26 17:33:42 +0800

    DRYing up

--- test/update_test.rb
+++ test/update_test.rb
@@ -2,32 +2,30 @@ require 'test_helper'
 
 class UpdateTest < Minitest::Test
   def setup
+    @item = Item.find_by(name: 'bomb')
+    @future = @item.updated_at + 1.day
   end
 
   def test_update_attribute
-    item = Item.find_by(name: 'bomb')
-
     in_sandbox do
-      Timecop.freeze(item.updated_at + 1.day) do
-        assert_equal true, item.atomically.update(name: 'bomb2')
+      Timecop.freeze(@future) do
+        assert_equal true, @item.atomically.update(name: 'bomb2')
 
-        new_item = Item.find_by(id: item.id)
+        new_item = Item.find_by(id: @item.id)
         assert_equal 'bomb2', new_item.name
-        assert_equal item.updated_at + 1.day, new_item.updated_at
+        assert_equal @future, new_item.updated_at
       end
     end
   end
 
   def test_update_attribute_to_same_value
-    item = Item.find_by(name: 'bomb')
-
     in_sandbox do
-      Timecop.freeze(item.updated_at + 1.day) do
-        assert_equal true, item.atomically.update(name: 'bomb')
+      Timecop.freeze(@future) do
+        assert_equal true, @item.atomically.update(name: 'bomb')
 
-        new_item = Item.find_by(id: item.id)
+        new_item = Item.find_by(id: @item.id)
         assert_equal 'bomb', new_item.name
-        assert_equal item.updated_at + 1.day, new_item.updated_at
+        assert_equal @future, new_item.updated_at
       end
     end
   end
