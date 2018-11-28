39d38dbd8b5df03d5c649196e964bc3d36789ab2
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-11-28 12:32:38 +0800

    add more test cases

--- test/update_all_test.rb
+++ test/update_all_test.rb
@@ -6,8 +6,30 @@ class UpdateAllTest < Minitest::Test
 
   def test_update_all_on_klass
     in_sandbox do
-      Item.atomically.update_all(3, name: '')
+      assert_equal 3, Item.atomically.update_all(3, name: '')
       assert_equal ['', '', ''], Item.pluck(:name)
+    end
+  end
+
+  def test_update_all_on_relation
+    in_sandbox do
+      assert_equal 2, Item.where(id: [1, 2]).atomically.update_all(2, name: '')
+      assert_equal ['', '', 'flame thrower'], Item.pluck(:name)
+    end
+  end
+
+  def test_update_all_on_relation_with_wrong_expected_size
+    in_sandbox do
+      assert_equal 0, Item.where(id: [1, 2]).atomically.update_all(3, name: '')
+      assert_equal ['bomb', 'water gun', 'flame thrower'], Item.pluck(:name)
+    end
+  end
+
+  def test_update_all_on_relation_and_with_race_condition
+    in_sandbox do
+      assert_equal 1, Item.where(id: 1).update_all(id: -1)
+      assert_equal 0, Item.where(id: [1, 2]).atomically.update_all(2, name: '')
+      assert_equal ['bomb', 'water gun', 'flame thrower'], Item.pluck(:name)
     end
   end
 end
