b43c6c2e1cb415744ee124e94ad8590ebb4671c9
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-12-20 17:51:41 +0800

    rename variable

--- lib/atomically/query_service.rb
+++ lib/atomically/query_service.rb
@@ -52,9 +52,9 @@ class Atomically::QueryService
     ids = nil
     id_column = "#{@klass.quoted_table_name}.#{quote_column(:id)}"
     @klass.transaction do
-      @relation.connection.execute('SET @id := NULL')
-      @relation.where("(SELECT @id := CONCAT_WS(',', #{id_column}, @id))").update_all(*args) # 撈出有真的被更新的 id，用逗號串在一起
-      ids = @klass.from(nil).pluck('@id').first
+      @relation.connection.execute('SET @ids := NULL')
+      @relation.where("(SELECT @ids := CONCAT_WS(',', #{id_column}, @ids))").update_all(*args) # 撈出有真的被更新的 id，用逗號串在一起
+      ids = @klass.from(nil).pluck('@ids').first
     end
     return ids.try{|s| s.split(',').map(&:to_i).uniq.sort } || [] # 將 id 從字串取出來 @id 的格式範例: '1,4,12'
   end
