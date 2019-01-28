35e6a90fb12d32546e814f1895679a3465977585
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-01-28 16:16:16 +0800

    test the number of queries

--- test/decrease_unsigned_counters_test.rb
+++ test/decrease_unsigned_counters_test.rb
@@ -9,7 +9,9 @@ class DecreaseUnsignedCountersTest < Minitest::Test
   def test_decrement_one_column_and_is_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
-      assert_equal true, user.atomically.decrement_unsigned_counters(money: 95)
+      assert_queries(1) do
+        assert_equal true, user.atomically.decrement_unsigned_counters(money: 95)
+      end
       assert_equal 5, user.reload.money
     end
   end
@@ -17,7 +19,9 @@ class DecreaseUnsignedCountersTest < Minitest::Test
   def test_decrement_one_column_and_is_not_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
-      assert_equal false, user.atomically.decrement_unsigned_counters(money: 105)
+      assert_queries(1) do
+        assert_equal false, user.atomically.decrement_unsigned_counters(money: 105)
+      end
       assert_equal 100, user.reload.money
     end
   end
@@ -26,7 +30,9 @@ class DecreaseUnsignedCountersTest < Minitest::Test
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
       User.where(id: user.id).update_all(money: 76)
-      assert_equal false, user.atomically.decrement_unsigned_counters(money: 105)
+      assert_queries(1) do
+        assert_equal false, user.atomically.decrement_unsigned_counters(money: 105)
+      end
       assert_equal 76, user.reload.money
     end
   end
@@ -34,7 +40,9 @@ class DecreaseUnsignedCountersTest < Minitest::Test
   def test_decrement_two_columns_and_all_are_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
-      assert_equal true, user.atomically.decrement_unsigned_counters(money: 95, action_point: 2)
+      assert_queries(1) do
+        assert_equal true, user.atomically.decrement_unsigned_counters(money: 95, action_point: 2)
+      end
       assert_equal 5, user.reload.money
       assert_equal 1, user.reload.action_point
     end
@@ -43,13 +51,17 @@ class DecreaseUnsignedCountersTest < Minitest::Test
   def test_decrement_two_columns_and_one_is_not_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
-      assert_equal false, user.atomically.decrement_unsigned_counters(money: 105, action_point: 2)
+      assert_queries(1) do
+        assert_equal false, user.atomically.decrement_unsigned_counters(money: 105, action_point: 2)
+      end
       assert_equal 100, user.reload.money
       assert_equal 3, user.reload.action_point
     end
 
     in_sandbox do
-      assert_equal false, user.atomically.decrement_unsigned_counters(money: 95, action_point: 4)
+      assert_queries(1) do
+        assert_equal false, user.atomically.decrement_unsigned_counters(money: 95, action_point: 4)
+      end
       assert_equal 100, user.reload.money
       assert_equal 3, user.reload.action_point
     end
@@ -58,7 +70,9 @@ class DecreaseUnsignedCountersTest < Minitest::Test
   def test_decrement_two_columns_and_all_are_not_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
-      assert_equal false, user.atomically.decrement_unsigned_counters(money: 105, action_point: 4)
+      assert_queries(1) do
+        assert_equal false, user.atomically.decrement_unsigned_counters(money: 105, action_point: 4)
+      end
       assert_equal 100, user.reload.money
       assert_equal 3, user.reload.action_point
     end
