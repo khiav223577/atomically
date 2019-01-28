7df702c803bf8a9a4912fa792785be37080cce34
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-01-28 15:24:48 +0800

    test decrease_counter

--- test/decrease_counter_test.rb
+++ test/decrease_counter_test.rb
@@ -6,7 +6,7 @@ class DecreaseCounterTest < Minitest::Test
   def setup
   end
 
-  def test_decrease_one_column_and_enough
+  def test_decrease_one_column_and_is_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
       assert_equal true, user.atomically.decrease_counter(money: 95)
@@ -14,11 +14,53 @@ class DecreaseCounterTest < Minitest::Test
     end
   end
 
-  def test_decrease_one_column_and_not_enough
+  def test_decrease_one_column_and_is_not_enough
     user = User.find_by(name: 'user_with_bomb_and_water_gun')
     in_sandbox do
       assert_equal false, user.atomically.decrease_counter(money: 105)
       assert_equal 100, user.reload.money
+    end
+  end
+
+  def test_decrease_one_column_and_was_enough
+    user = User.find_by(name: 'user_with_bomb_and_water_gun')
+    in_sandbox do
+      User.where(id: user.id).update_all(money: 76)
+      assert_equal false, user.atomically.decrease_counter(money: 105)
+      assert_equal 76, user.reload.money
+    end
+  end
+
+  def test_decrease_two_columns_and_all_are_enough
+    user = User.find_by(name: 'user_with_bomb_and_water_gun')
+    in_sandbox do
+      assert_equal true, user.atomically.decrease_counter(money: 95, action_point: 2)
+      assert_equal 5, user.reload.money
+      assert_equal 1, user.reload.action_point
+    end
+  end
+
+  def test_decrease_two_columns_and_one_is_not_enough
+    user = User.find_by(name: 'user_with_bomb_and_water_gun')
+    in_sandbox do
+      assert_equal false, user.atomically.decrease_counter(money: 105, action_point: 2)
+      assert_equal 100, user.reload.money
+      assert_equal 3, user.reload.action_point
+    end
+
+    in_sandbox do
+      assert_equal false, user.atomically.decrease_counter(money: 95, action_point: 4)
+      assert_equal 100, user.reload.money
+      assert_equal 3, user.reload.action_point
+    end
+  end
+
+  def test_decrease_two_columns_and_all_are_not_enough
+    user = User.find_by(name: 'user_with_bomb_and_water_gun')
+    in_sandbox do
+      assert_equal false, user.atomically.decrease_counter(money: 105, action_point: 4)
+      assert_equal 100, user.reload.money
+      assert_equal 3, user.reload.action_point
     end
   end
 end
