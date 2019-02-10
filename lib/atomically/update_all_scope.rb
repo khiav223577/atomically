28a5994d14b42fcd5f275da081095fbad1f085bd
Rumble Huang <mrtmrt15xn@yahoo.com.tw>
2019-02-10 20:00:09 +0800

    calculate raw update query

--- lib/atomically/update_all_scope.rb
+++ lib/atomically/update_all_scope.rb
@@ -25,4 +25,27 @@ class UpdateAllScope
   def klass
     @relation.klass
   end
+
+  # See: https://github.com/rails/rails/blob/fc5dd0b85189811062c85520fd70de8389b55aeb/activerecord/lib/active_record/relation.rb#L315
+  def to_update_manager
+    stmt = Arel::UpdateManager.new
+
+    stmt.set Arel.sql(@queries.join(','))
+    stmt.table(@relation.table)
+
+    if @relation.send(:has_join_values?) || @relation.offset_value
+      klass.connection.join_to_update(stmt, @relation.arel, @relation.arel_attribute(@relation.primary_key))
+    else
+      stmt.key = @relation.arel_attribute(@relation.primary_key)
+      stmt.take(@relation.arel.limit)
+      stmt.order(*@relation.arel.orders)
+      stmt.wheres = @relation.arel.constraints
+    end
+
+    return stmt
+  end
+
+  def to_sql
+    to_update_manager.to_sql
+  end
 end
